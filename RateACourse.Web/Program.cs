86a4884dd61f11b041b1d8bcc37276b080924c20
Miledi75 <mileto.di.marco@howest.be>
2023-06-22 15:30:20 +0200

    Add project files.

---
+++ RateACourse.Web/Program.cs
@@ -0,0 +1,43 @@
+using RateACourse.Web.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace RateAMovie_opl_Afst
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            //Add entity framework database
+            builder.Services.AddDbContext<CourseRateDbContext>(
+                 options => options.UseSqlServer(builder.Configuration.GetConnectionString("CourseRateDb")));
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
